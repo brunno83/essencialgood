09e6378d06aadc7a84b50ed62b5be52543806449
unknown <contatodiegoabrantes@gmail.com>
2026-08-27 10:50:13 -0300

    update section 3 image to 3:4 version with English text

--- src/components/pdp/IngredientsSection.jsx
+++ src/components/pdp/IngredientsSection.jsx
@@ -55,7 +55,7 @@ export function IngredientsSection({ ingredientsSection, accentColor }) {
                 width: '100%',
                 height: 'auto',
                 aspectRatio: ingredientsSection.aspectRatio || '3 / 4',
-                objectFit: 'contain',
+                objectFit: 'cover',
                 display: 'block'
               }}
             />
